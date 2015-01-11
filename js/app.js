5b6a303c396ad938e5b04aa3c7409ab6b8657e56
Chris Z <christopher.ziemer@gmail.com>
2015-01-11 12:05:23 -0500

    Player and 1 Enemy Rendered
    player can move about board - can still leave board. No enemy animation,
    but it rendered.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 101;
+    this.y = 239;
+    this.speed = 25;
 }
 
 // Update the enemy's position, required method for game
@@ -25,11 +28,50 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// The Player character
+var Player = function () {
+	this.sprite = 'images/char-boy.png';
+	this.x = 101;
+    this.y = 405;
+}
+
+// Update the players's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the player on the screen, required method for game
+// Same as the Enemy prototype above
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(direction) {
+	if (direction === 'up') {
+		this.y -= 83;
+	}
+	else if (direction === 'down') {
+		this.y += 83;
+	}
+	else if (direction === 'left') {
+		this.x -= 101;
+	}
+	else if (direction === 'right') {
+		this.x += 101;
+	}
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+
+var allEnemies = [new Enemy()];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
